fcc03efd63d31fc2482dc94054900b899a29116b
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-07-12 14:00:43 -0300

    feat: create endpoint to to confirm participant

--- src/main/java/com/rocketseat/planner/participant/ParticipantService.java
+++ src/main/java/com/rocketseat/planner/participant/ParticipantService.java
@@ -1,14 +1,25 @@
 package com.rocketseat.planner.participant;
 
+import com.rocketseat.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ParticipantService {
-    public void registerParticipantToEvent(List<String> participantsToInvite, UUID tripId) {
 
+    @Autowired
+    private ParticipantRepository repository;
+
+    public void registerParticipantToEvent(List<String> participantsToInvite, Trip trip) {
+        List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).collect(Collectors.toList());
+
+        this.repository.saveAll(participants);
+
+        System.out.println(participants.get(0).getId());
     }
 
     public void triggerConfirmationEmailToParticipant(UUID tripId) {}
